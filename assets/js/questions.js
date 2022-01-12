63422b012d73be5aa5b25eb81ad20d5e2d336f49
Pauline Rugwevera <prugwevera28@gmail.com>
2022-01-12 17:39:10 +0000

    Add contents to questions page

---
+++ assets/js/questions.js
@@ -0,0 +1,275 @@
+const foodandWine = [
+    {
+        question : ' What beans are used to make baked beans?',
+        a : 'Borlotti beans',
+        b :'Haricot beans',
+        c: 'Cannellini beans',
+        d: 'Flageolet beans',
+       answer : 'b'
+    },
+    {
+    
+        question : 'What does IPA stand for?',
+        a : 'Intensely potent ale',
+        b :'India pale ale',
+        c: 'India premium ale',
+        d: 'International porter association',
+       answer : 'b'
+       
+    },
+
+    {
+        question : 'Bobotie is a dish from country?',
+        a : 'Algeria',
+        b :'Argentina',
+        c: 'Ireland',
+        d: 'South Africa',
+       answer : 'd'
+
+    },
+    {
+        question : 'Egusi is dish popular in which country?',
+        a : 'Algeria',
+        b :'Nigeria',
+        c: 'Ireland',
+        d: 'South Africa',
+       answer : 'b'
+
+    },
+    {
+        question : 'What do you serve nigiri with?',
+        a : 'dumplings',
+        b :'chopsticks',
+        c: 'fish',
+        d: 'Sushi with rice and sea food',
+       answer : 'd'
+
+    },
+    {
+        question : 'Suya spice is a spice popular in which country?',
+        a : 'UAE',
+        b :'Singapore',
+        c: 'Nigeria',
+        d: 'Quatar',
+       answer : 'c'
+
+    },
+    {
+        question : 'Which flower does the spice saffron come from?',
+        a : 'Crocus',
+        b :'Orchid',
+        c: 'Poppy',
+        d: 'Tulip',
+       answer : 'a'
+
+    },
+    {
+        question : 'What is used to flavour a whityby bun',
+        a : 'sorghum',
+        b :'almond',
+        c: 'orange',
+        d: 'lemon',
+       answer : 'd'
+
+    },
+    {
+        question: 'Gruyère is a cheese from what nation?',
+        a : 'Switzerland',
+        b :'UAE',
+        c: 'Italy',
+        d: 'Germany',
+       answer : 'a'
+
+    },
+    {
+        question: ' What ingredients are in the dish Cullen Skink?',
+        a : 'fish and potatoes',
+        b :'haddock and potatoes',
+        c: 'Baramundi and potatoes',
+        d: 'Salmon and potatoes',
+       answer : 'b'
+
+    },
+    {
+        question: ' Which of the following cocktails does NOT include gin?',
+        a : 'Cosmopolitan',
+        b :'Gimlet',
+        c: 'Collins',
+        d: 'Bramble',
+       answer : 'a'
+
+    },
+    {
+        question: ' A Magnum holds two standard bottles of champagne, a Réhoboam holds six, but how many bottles of champagne in a Nebuchadnezzar?',
+        a : '20',
+        b :'0',
+        c: '2',
+        d: '25',
+       answer : 'a'
+
+    },
+    
+    {
+        question: 'When did the first Masterchef episode appear on TV',
+        a:'1989',
+        b :'2000',
+        c: '1996',
+        d: '1990',
+       answer : 'd'
+
+    },
+    {
+        question: 'Roughly how much does an egg (in its shell) weigh?',
+        a:'20g',
+        b :'55g',
+        c: '23g',
+        d: '40g',
+       answer : 'b'
+
+    },
+    {
+        question: 'Which MasterChef presenter was formerly a green grocer?',
+        a:'Don Brooke',
+        b :'Gordon Ramsey',
+        c: 'Gregg Wallace',
+        d: 'John Torode',
+       answer : 'c'
+
+    },
+    {
+        question: ' What is the most consumed manufactured drink in the world?',
+        a:'coffee',
+        b :'champaigne',
+        c: 'tea',
+        d: 'water',
+       answer : 'c'
+
+    },
+    {
+        question: ' What is the most expensive spice in the world by weight?',
+        a:'chinese 5 spice',
+        b :'blackpepper',
+        c: 'italian herbs',
+        d: 'saffron',
+       answer : 'd'
+
+    },
+    {
+        question: ' What does it mean when a wine is said to be hot?',
+        a:'hot to handle',
+        b :'high in alcohol',
+        c: 'high in cholestrol',
+        d: 'burning sensation',
+       answer : 'b'
+
+    },
+    {
+        question: ' What is the principal acid in grapes that promotes aging and flavour?',
+        a:'Tartaric acid',
+        b :'citric',
+        c: 'Sorbic',
+        d: 'Lactic',
+       answer : 'a'
+
+    },
+    {
+        question: 'Retsina is a wine from which country?',
+        a:'Germany',
+        b :'France',
+        c: 'Greece',
+        d: 'Egypt',
+       answer : 'c'
+
+    },
+    {
+        question: 'Retsina is a wine from which country?',
+        a:'Germany',
+        b :'France',
+        c: 'Greece',
+        d: 'Egypt',
+       answer : 'c'
+
+    },
+    {
+        question: 'Hawke Bay is the oldest wine region in which country?',
+        a:'UAE',
+        b :'Singapore',
+        c: 'New Zealand',
+        d: 'Morocco',
+       answer : 'c'
+
+    },
+    {
+        question: 'Hawke Bay is the oldest wine region in which country?',
+        a:'UAE',
+        b :'Singapore',
+        c: 'New Zealand',
+        d: 'Morocco',
+       answer : 'c'
+
+    },
+    {
+        question: 'How many litres does a magnum wine bottle hold?',
+        a:'1.5',
+        b :'0.5',
+        c: '0.3',
+        d: '2',
+       answer : 'a'
+
+    },
+    {
+        question: 'What name is given to the production, study and science of grapes?',
+        a:'Oenological',
+        b: 'Horticulture',
+        c: 'Oenology',
+        d: 'Viticulture ',
+       answer : 'd'
+
+    },
+    {
+        question: 'What type of fruit is a Banana',
+        a:'Berry',
+        b: 'Drupe',
+        c: 'Pepo',
+        d: 'herb ',
+       answer : 'a'
+
+    },
+    {
+        question: 'which German desert is thin sheet of rolled up dough baked?',
+        a:'apple pie',
+        b: 'Strudel',
+        c: 'Quark balls',
+        d: 'Croissants',
+       answer : 'b'
+
+    },
+    {
+        question: 'Which of the following countries produces cocoa',
+        a:'Egypt',
+        b: 'South Africa',
+        c: 'Cote Divoire',
+        d: 'Germany',
+       answer : 'c'
+
+    },
+    {
+        question: 'Which of the following countries produces cocoa',
+        a:'Egypt',
+        b: 'South Africa',
+        c: 'Cote Divoire',
+        d: 'Germany',
+       answer : 'c'
+
+    },
+    {
+        question: 'Peanut butter rice is popular dish in which country',
+        a:'Zimbabwe',
+        b: 'South Africa',
+        c: 'Lesotho',
+        d: 'Zambia',
+       answer : 'c'
+
+    },
+
+]
