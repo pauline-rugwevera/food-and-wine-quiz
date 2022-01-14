54d2ce1da10e7caaaea11812ca25d27487e39739
Pauline Rugwevera <prugwevera28@gmail.com>
2022-01-14 10:57:05 +0000

    Change id name to start

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 let startBtn = document.getElementById('start-btn');
 startBtn.addEventListener('click', function(){
-    document.getElementById('welcome') .classList.add('hide');
+    document.getElementById('start') .classList.add('hide');
     document.getElementById('results-section').classList.add('hide');
     document.getElementById('question-container').classList.remove('hide');
     
