bf38aceea760a5d93b7b1035526c02486c9618fb
Pauline Rugwevera <prugwevera28@gmail.com>
2022-02-07 11:17:03 +0000

    Clean and update questions file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,267 +1,265 @@
-
-let foodAndWine = [
-    {
-        question : ' What beans are used to make baked beans?',
-        a : 'Borlotti beans',
-        b :'Haricot beans',
+let foodAndWine = [{
+        question: ' What beans are used to make baked beans?',
+        a: 'Borlotti beans',
+        b: 'Haricot beans',
         c: 'Cannellini beans',
         d: 'Flageolet beans',
-       answer : 'b'
+        answer: 'b'
     },
     {
-    
-        question : 'What does IPA stand for?',
-        a : 'Intensely potent ale',
-        b :'India pale ale',
+
+        question: 'What does IPA stand for?',
+        a: 'Intensely potent ale',
+        b: 'India pale ale',
         c: 'India premium ale',
         d: 'International porter association',
-       answer : 'b'
-       
+        answer: 'b'
+
     },
 
     {
-        question : 'Bobotie is a dish from country?',
-        a : 'Algeria',
-        b :'Argentina',
+        question: 'Bobotie is a dish from country?',
+        a: 'Algeria',
+        b: 'Argentina',
         c: 'Ireland',
         d: 'South Africa',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
-        question : 'Egusi is dish popular in which country?',
-        a : 'Algeria',
-        b :'Nigeria',
+        question: 'Egusi is dish popular in which country?',
+        a: 'Algeria',
+        b: 'Nigeria',
         c: 'Ireland',
         d: 'South Africa',
-       answer : 'b'
+        answer: 'b'
 
     },
     {
-        question : 'What do you serve nigiri with?',
-        a : 'dumplings',
-        b :'chopsticks',
+        question: 'What do you serve nigiri with?',
+        a: 'dumplings',
+        b: 'chopsticks',
         c: 'fish',
         d: 'Sushi with rice and sea food',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
-        question : 'Suya spice is a spice popular in which country?',
-        a : 'UAE',
-        b :'Singapore',
+        question: 'Suya spice is a spice popular in which country?',
+        a: 'UAE',
+        b: 'Singapore',
         c: 'Nigeria',
         d: 'Quatar',
-       answer : 'c'
+        answer: 'c'
 
     },
     {
-        question : 'Which flower does the spice saffron come from?',
-        a : 'Crocus',
-        b :'Orchid',
+        question: 'Which flower does the spice saffron come from?',
+        a: 'Crocus',
+        b: 'Orchid',
         c: 'Poppy',
         d: 'Tulip',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
-        question : 'What is used to flavour a whityby bun',
-        a : 'sorghum',
-        b :'almond',
+        question: 'What is used to flavour a whityby bun',
+        a: 'sorghum',
+        b: 'almond',
         c: 'orange',
         d: 'lemon',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
         question: 'Gruyère is a cheese from what nation?',
-        a : 'Switzerland',
-        b :'UAE',
+        a: 'Switzerland',
+        b: 'UAE',
         c: 'Italy',
         d: 'Germany',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: ' What ingredients are in the dish Cullen Skink?',
-        a : 'fish & potatoes',
-        b :'haddock & potatoes',
+        a: 'fish & potatoes',
+        b: 'haddock & potatoes',
         c: 'Baramundi & potatoes',
         d: 'Salmon & potatoes',
-       answer : 'b'
+        answer: 'b'
 
     },
     {
         question: ' Which of the following cocktails does NOT include gin?',
-        a : 'Cosmopolitan',
-        b :'Gimlet',
+        a: 'Cosmopolitan',
+        b: 'Gimlet',
         c: 'Collins',
         d: 'Bramble',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
-        question:  'What is the most popular Asian dish',
-        a : 'Fried rice',
-        b :'Sweet & sour pork',
+        question: 'What is the most popular Asian dish',
+        a: 'Fried rice',
+        b: 'Sweet & sour pork',
         c: 'Peking duck',
         d: 'Teriyaki',
-       answer : 'c'
+        answer: 'c'
 
     },
-    
+
     {
         question: 'How much does an egg weigh?',
-        a:'20g',
-        b :'55g',
+        a: '20g',
+        b: '55g',
         c: '23g',
         d: '40g',
-       answer : 'b'
+        answer: 'b'
 
     },
 
     {
         question: ' What is the most expensive spice in the world?',
-        a:'chinese 5 spice',
-        b :'blackpepper',
+        a: 'chinese 5 spice',
+        b: 'blackpepper',
         c: 'italian herbs',
         d: 'saffron',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
         question: ' What does it mean when a wine is said to be hot?',
-        a:'hot to handle',
-        b :'high in alcohol',
+        a: 'hot to handle',
+        b: 'high in alcohol',
         c: 'high in cholestrol',
         d: 'burning sensation',
-       answer : 'b'
+        answer: 'b'
 
     },
     {
         question: ' What is the acid in grapes that promotes aging and flavour?',
-        a:'Tartaric acid',
-        b :'citric',
+        a: 'Tartaric acid',
+        b: 'citric',
         c: 'Sorbic',
         d: 'Lactic',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: ' Shirley, Ailsa Craig and Mr Stripey are all types of what?',
-        a:'Zuccini',
-        b :'Apple',
+        a: 'Zuccini',
+        b: 'Apple',
         c: 'Tomato',
         d: 'Pumpkin',
-       answer : 'c'
+        answer: 'c'
 
     },
     {
         question: 'Retsina is a wine from which country?',
-        a:'Germany',
-        b :'France',
+        a: 'Germany',
+        b: 'France',
         c: 'Greece',
         d: 'Egypt',
-       answer : 'c'
+        answer: 'c'
 
     },
     {
         question: ' What is a spoot?',
-        a:'A type of clam',
-        b :'Potato',
+        a: 'A type of clam',
+        b: 'Potato',
         c: 'Kitchen Mixer',
         d: 'Masher',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: 'Hawke Bay is the oldest wine region in which country?',
-        a:'UAE',
-        b :'Singapore',
+        a: 'UAE',
+        b: 'Singapore',
         c: 'New Zealand',
         d: 'Morocco',
-       answer : 'c'
+        answer: 'c'
 
     },
     {
         question: 'How many litres does a magnum wine bottle hold?',
-        a:'1.5',
-        b :'0.5',
+        a: '1.5',
+        b: '0.5',
         c: '0.3',
         d: '2',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: 'What is the production, study and science of grapes?',
-        a:'Oenological',
+        a: 'Oenological',
         b: 'Horticulture',
         c: 'Oenology',
         d: 'Viticulture ',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
         question: 'What type of fruit is a Banana',
-        a:'Berry',
+        a: 'Berry',
         b: 'Drupe',
         c: 'Pepo',
         d: 'herb ',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: 'which German desert is thin sheet of rolled up dough baked?',
-        a:'apple pie',
+        a: 'apple pie',
         b: 'Strudel',
         c: 'Quark balls',
         d: 'Croissants',
-       answer : 'b'
+        answer: 'b'
 
     },
     {
         question: '..is one of the ingredients of Worcestershire Sauce?',
-        a:'Tamarind',
+        a: 'Tamarind',
         b: 'Chinese 5 spice',
         c: 'Soy sauce',
         d: 'Tomato',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
         question: 'Which of the following countries produces cocoa',
-        a:'Egypt',
+        a: 'Egypt',
         b: 'South Africa',
         c: 'Cote Divoire',
         d: 'Germany',
-       answer : 'c'
+        answer: 'c'
 
     },
     {
         question: 'Peanut butter rice is popular dish in which country',
-        a:'Zimbabwe',
+        a: 'Zimbabwe',
         b: 'South Africa',
         c: 'Lesotho',
         d: 'Zambia',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
         question: 'Jollof rice is national dish of which country',
-        a:'Ghana',
+        a: 'Ghana',
         b: 'South Africa',
         c: 'Egypt',
         d: 'Nigeria',
-       answer : 'd'
+        answer: 'd'
 
     },
     {
         question: 'Marzipan is derived from?',
-        a:'Almond',
+        a: 'Almond',
         b: 'Round Nut',
         c: 'Pecans',
         d: 'Brazillian nut',
-       answer : 'a'
+        answer: 'a'
 
     },
     {
@@ -270,7 +268,7 @@ let foodAndWine = [
         b: 'Parsely',
         c: 'Basil',
         d: 'Pine nuts',
-        answer : 'c'
+        answer: 'c'
     },
     {
         question: 'Lectin protein is found in',
@@ -278,7 +276,7 @@ let foodAndWine = [
         b: 'Rice',
         c: 'Milk',
         d: 'sorghum',
-        answer : 'a'
+        answer: 'a'
     },
     {
         question: 'Caesar salad originate from',
@@ -286,7 +284,7 @@ let foodAndWine = [
         b: 'Canada',
         c: 'Mexico',
         d: 'None of the above',
-        answer : 'c'
+        answer: 'c'
     },
     {
         question: 'Which of the following is the richest source of vitamin C',
@@ -294,7 +292,7 @@ let foodAndWine = [
         b: 'Milk',
         c: 'Rice',
         d: 'Cantaloupe',
-        answer : 'd'
+        answer: 'd'
     }
 
 ]
